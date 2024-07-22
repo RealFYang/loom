568170c167566468f5b6c25bb3824b74b63c413c
Fei Yang <yangfei@iscas.ac.cn>
2024-07-22 16:36:28 +0800

    RISC-V: Remove debugging code

--- src/java.base/share/classes/java/lang/VirtualThread.java
+++ src/java.base/share/classes/java/lang/VirtualThread.java
@@ -1011,9 +1011,6 @@ final class VirtualThread extends BaseVirtualThread {
         boolean yielded = false;
         try {
             yielded = yieldContinuation();  // may throw
-            if (!yielded) {
-                throw new RuntimeException("Failed to yield vthread!");
-            }
         } finally {
             assert (Thread.currentThread() == this) && (yielded == (state() == RUNNING));
             if (!yielded) {
